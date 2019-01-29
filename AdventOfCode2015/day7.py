d910dfb427999393098ad62ec770d5c3d0b1942f
soeptas <boebievdd@gmail.com>
2019-01-29 12:03:59 +0100

    finish day 7

--- AdventOfCode2015/day7.py
+++ AdventOfCode2015/day7.py
@@ -1,57 +1,13 @@
 
-circuit = ["123 -> x", "456 -> y", "x AND y -> d", "x OR y -> e", "x LSHIFT 2 -> f", "y RSHIFT 2 -> g", "NOT x -> h", "NOT y -> i"]
+lines = open('inputDay7.txt', 'r')
+
+circuit = []
+for rule in lines:
+	circuit += [rule]
+
+lines.close()
 
 ## Part 1
-'''
-notDict = {}
-andDict = {}
-orDict = {}
-lshiftDict = {}
-rshiftDict = {}
-varDict = {}
-
-
-for line in circuit:
-	splittedLine = line.rstrip().split['->']
-	if "NOT" in line:
-		notDict[splittedLine[0].split(' ')[1]] = splittedLine[1]
-	elif "AND" in line or "OR" in line or "LSHIFT" in line or "RSHIFT" in line:
-		(x,y) = (splittedLine[0].split(' ')[0],splittedLine[0].split(' ')[2])
-		if "AND" in line:
-			andDict[(x,y)] = splittedLine[1]
-		elif "OR" in line:
-			orDict[(x,y)] = splittedLine[1]
-		elif "LSHIFT" in line:
-			lshiftDict[(x,y)] = splittedLine[1]
-		elif "RSHIFT" in line:
-			rshiftDict[(x,y)] = splittedLine[1]
-	else:
-		varDict[splittedLine[1]] = int(splittedLine[0])
-
-def completeVars():
-	dicts = [notDict, andDict, orDict, lshiftDict, rshiftDict]
-	return dicts
-
-# Recursief rule finding algo die dat dan toepast zou chaud zijn
-def findRulesElements(el):
-	dicts = completeVars()
-	for subdict in dicts:
-		for key, value in subdict.items():
-			if value == el:
-				
-
-def find(var):
-	toFind = [var]
-	while len(toFind) > 0:
-		newToFind = []
-		for el in toFind:
-			if el not in varDict:
-				newToFind += findRulesElements(el)
-		toFind = newToFind
-	
-	return varDict[var]
-'''
-
 varDict = {}
 
 def findRule(var):
@@ -59,17 +15,19 @@ def findRule(var):
 		if rule.rstrip().split(' ')[-1] == var:
 			return rule
 
-# TODO fix overflow en underflow in 16-bits
+# https://wiki.python.org/moin/BitwiseOperators
 def find(var):
 	if var in varDict:
 		return varDict[var]
-	if var is int:
-		return var # Makes RSHIFT and LSHIFT work
-	rule = findRule(var)
-	print(rule)
+	try:
+		var = int(var) # Makes RSHIFT and LSHIFT work
+		return var
+	except ValueError:
+		pass
+	rule = findRule(var)	
 	splittedRule = rule.rstrip().split('->')
 	if "NOT" in rule:
-		return ~ find(splittedRule[0].split(' ')[1])
+		varDict[var] = ~ find(splittedRule[0].split(' ')[1])
 	elif "AND" in rule or "OR" in rule or "LSHIFT" in rule or "RSHIFT" in rule:
 		(x,y) = (splittedRule[0].split(' ')[0],splittedRule[0].split(' ')[2])
 		if "AND" in rule:
@@ -78,12 +36,35 @@ def find(var):
 			binaryOperator = lambda x, y : x | y
 		elif "LSHIFT" in rule:
 			binaryOperator = lambda x, y : x << y
-			return find(x) 
 		elif "RSHIFT" in rule:
 			binaryOperator = lambda x, y : x >> y
-		return binaryOperator(find(x), find(y))
+		varDict[var] = binaryOperator(find(x), find(y))
 	else:
-		varDict[var] = int(splittedRule[0])
-		return varDict[var]
+		try:
+			value = int(splittedRule[0])
+			varDict[var] = value
+		except ValueError:
+			varDict[var] = find(splittedRule[0].strip())
+	return varDict[var]
+			
+print(find('a') % 65536) # Because we work in 16-bit representation
 
-print(find("i"))
+
+## Part 2
+def override(circuit, value, var):
+	rule = findRule(var)
+	circuit.remove(rule)
+	circuit += [str(value) + " -> " + var]
+	return circuit
+
+circuit = override(circuit, find('a'), 'b')
+
+varDict = {}
+print(find('a') % 65536) # Because we work in 16-bit representation
+
+
+# NOTE
+# I only performed the modulo operation in the end, to avoid the overhead of running
+# this operation on each variable in the recursive function.
+# This results in the same output, and since my computer does not work in 16-bit 
+# representation it is not necessary to perform all operations on 16-bit numbers.
