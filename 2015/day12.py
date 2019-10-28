9d0611a9825afc7c2cc540c607713b3348c7bf74
soeptas <boebievdd@gmail.com>
2019-10-28 21:13:09 +0100

    complete day12

--- 2015/day12.py
+++ 2015/day12.py
@@ -1,31 +1,32 @@
 
 import re
+import json
 
-book = open('input/inputDay12.txt', 'r')
+#book = open('input/inputDay12.txt', 'r')
+
+with open('input/inputDay12.txt') as f:
+	book = json.load(f)
 
 ## Part 1
-total = 0
-for line in book:
-	numbers = re.findall(r'-?\d+', line)
-	total += sum(map(int, numbers))
+numbers = re.findall(r'-?\d+', str(book))
+total = sum(map(int, numbers))
 
 print(total)
 
 
 ## Part 2
-book.seek(0)
-wrong = 0
-counter = 0
-for line in book:
-	counter += 1
-	print(len(line))
-	wrongParts = re.findall(r'{.*red.*}', line)
-	print(len(wrongParts[0]))
-	wrongNumbers = [re.findall(r'-?\d+', part) for part in wrongParts]
-	wrong += sum(map(int, wrongNumbers[0]))
+def item_sum(item):
+	if isinstance(item, list):
+		return sum([item_sum(i) for i in item])
+	elif isinstance(item, dict):
+		if 'red' in item.values():
+			return 0
+		return sum([item_sum(i) for i in item.values()])
+	elif isinstance(item, int):
+		return item
+	else: # Return 0 for other items
+		return 0
 
-total = total - wrong
+total = item_sum(book)
+
 print(total)
-print(counter)
-
-book.close()
